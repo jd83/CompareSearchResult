b5c888a3f9c96e503a6734f3883d8d900f1d0427
jd83 <flyincoldwind@aliyun.com>
2017-05-26 01:39:47 +0800

    Version 0.1 finish.

--- src/compare/CompareResult.java
+++ src/compare/CompareResult.java
@@ -1,70 +1,65 @@
 package compare;
 
+import java.io.IOException;
 import java.math.BigDecimal;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.ArrayList;
 import java.util.List;
 
-import org.htmlparser.Node;
-import org.htmlparser.Parser;
-import org.htmlparser.filters.HasAttributeFilter;
-import org.htmlparser.util.NodeList;
 import org.htmlparser.util.ParserException;
 
 public class CompareResult {
 
 	public static void main(String[] args) throws Exception {
 		CompareResult objCompareResult = new CompareResult();
-		objCompareResult.searchByBaidu("蚂蚁");
+		objCompareResult.testEnglishSearch();
+		objCompareResult.testChineseSearch();
 
 	}
 	
-	public void searchByBaidu(String key) throws Exception {
-		String urlBaidu = String.format("http://www.baidu.com/s?wd=%s", key);
-		URLConnection connection = getUrlConnection(urlBaidu, key);
-		getSearchedUrlsByBaidu(connection);
-	}
-
-	public void searchByGoogle(String key) throws Exception {
-		String urlGoogle = String
-				.format("https://www.google.com.tw/#q=%s", key);
-		getUrlConnection(urlGoogle, key);
-	}
-
-	public URLConnection getUrlConnection(String site, String key)
-			throws Exception {
-		URL url = new URL(site);
-		URLConnection rc = url.openConnection();
-		return rc;
-	}
-
-	public List<String> getSearchedUrlsByBaidu(URLConnection rc)
-			throws ParserException {
-		Parser resParser = new Parser((HttpURLConnection) rc);
-		HasAttributeFilter attribute2Filter = new HasAttributeFilter("class",
-				"c-showurl");
-		NodeList ndList = resParser.extractAllNodesThatMatch(attribute2Filter);
-		List<String> urlList = new ArrayList<String>();
-		for (int i = 0; i < ndList.size(); i++) {
-			Node subNode = ndList.elementAt(i);
-			urlList.add(subNode.toPlainTextString().replaceAll("&nbsp;", ""));
-
-		}
-		return urlList;
+	/**
+ * 测试英文搜索到搜索结果重合率
+ * */
+	public void testEnglishSearch() throws ParserException, IOException {
+		String key = "test";
+		BigDecimal percent = testSiteLike(key);
+		System.out.println(String.format("English search: Similarity:%s%%",
+				percent.toString()));
 	}
 	
-	public BigDecimal getSimailarityOfList(List<String>lstSrc,List<String>lstDes){		
+	/**
+ * 测试中文搜索到搜索结果重合率
+ * */
+	public void testChineseSearch() throws ParserException, IOException {
+		String key = "蚂蚁";
+		BigDecimal percent = testSiteLike(key);
+		System.out.println(String.format("Chinese search: Similarity:%s%%",
+				percent.toString()));
+	}
+	
+	/**
+ * 返回百度和谷歌搜索结果链接地址的相似度
+ * */
+	public BigDecimal testSiteLike(String key) throws ParserException,
+			IOException {
+		List<String> baiduList = ContentHelper.getSiteByBaidu(key);
+		List<String> googleList = ContentHelper.getSiteByGoogle(key);
+		BigDecimal percent = getSimilarityOfList(baiduList, googleList);
+		return percent;
+	}
+	
+	/**
+ * 返回两个List的相似度
+ * */
+	public BigDecimal getSimilarityOfList(List<String> lstSrc,
+			List<String> lstDes) {
 		lstSrc.retainAll(lstDes);
-		
+
 		BigDecimal percent;
-		BigDecimal sameDecimal=new BigDecimal(lstSrc.size());
-		BigDecimal totalDecimal=new BigDecimal(lstDes.size());
-			
-		percent=sameDecimal.multiply(new BigDecimal(100)).divide(totalDecimal,2,BigDecimal.ROUND_HALF_UP);
-		return percent;	
+		BigDecimal sameDecimal = new BigDecimal(lstSrc.size());
+		BigDecimal totalDecimal = new BigDecimal(lstDes.size());
+
+		percent = sameDecimal.multiply(new BigDecimal(100)).divide(
+				totalDecimal, 2, BigDecimal.ROUND_HALF_UP);
+		return percent;
 	}
-	
-	
+
 }
--- src/compare/ContentHelper.java
+++ src/compare/ContentHelper.java
@@ -1,25 +1,107 @@
 package compare;
 
-import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ContentHelper{
-	public static void main(String[] args) throws IOException {
-		getContentByPhantomjs("htt2p://www.baidu.com","");
+import net.anthavio.phanbedder.Phanbedder;
+
+import org.htmlparser.Node;
+import org.htmlparser.NodeFilter;
+import org.htmlparser.Parser;
+import org.htmlparser.filters.HasAttributeFilter;
+import org.htmlparser.util.NodeList;
+import org.htmlparser.util.ParserException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class ContentHelper {
+
+	/**
+ * 获取百度搜索到的链接
+ * */
+	public static List<String> getSiteByBaidu(String key) throws IOException,
+			ParserException {
+		String url = String.format("http://www.baidu.com/s?wd=%s", key);
+		By queryName = null;
+		String resContent = getContentByPhantomjs(url, key, queryName);
+		//对网页返回内容进行过滤，查找链接地址的节点
+		HasAttributeFilter addressFilter = new HasAttributeFilter("class",
+				"c-showurl");
+		List<String> list = parseContent(resContent, addressFilter);
+
+		return list;
 	}
-	public static String getContentByPhantomjs(String site, String key) throws IOException {		
-		Runtime rt= Runtime.getRuntime();
-		Process p = rt.exec("/CompareSearchResult/lib/phantomjs.exe /CompareSearchResult/resource/code.js "+site);
-		InputStream inputStream = p.getInputStream();     
-        BufferedReader bReader = new BufferedReader(new InputStreamReader(inputStream));     
-        StringBuffer sBuffer = new StringBuffer();     
-        String tmp = "";     
-        while((tmp = bReader.readLine())!=null){     
-            sBuffer.append(tmp);     
-        }        
-        System.out.println(sBuffer.toString());
-        return sBuffer.toString();     
+
+	/**
+ * 获取谷歌搜索到的链接
+ * */
+	public static List<String> getSiteByGoogle(String key) throws IOException,
+			ParserException {
+		String url = "https://www.google.com";
+		By queryName = By.name("q");
+		String resContent = getContentByPhantomjs(url, key, queryName);
+		//对网页返回内容进行过滤，查找链接地址的节点
+		HasAttributeFilter addressFilter = new HasAttributeFilter("class", "kv");
+		List<String> list = parseContent(resContent, addressFilter);
+
+		return list;
+	}
+
+	/**
+ * 对网页返回内容根据过滤器进行过滤
+ * */
+	public static List<String> parseContent(String content,
+			NodeFilter nodeFilter) throws ParserException {
+		Parser resParser = new Parser(content);
+		NodeList ndList = resParser.extractAllNodesThatMatch(nodeFilter);
+		List<String> urlList = new ArrayList<String>();
+		for (int i = 0; i < ndList.size(); i++) {
+			Node subNode = ndList.elementAt(i);
+			String site = subNode.toPlainTextString();
+			site = getSiteName(site);
+			urlList.add(site);
+		}
+		return urlList;
+	}
+
+	public static String getContentByPhantomjs(String url, String key, By by)
+			throws IOException {
+		PhantomJSDriver driver = getDriver();
+		driver.get(url);
+		if (by != null) {
+			WebElement query = driver.findElement(by);
+			query.sendKeys(key);
+			query.submit();
+		}
+		String rsString = driver.getPageSource();
+		driver.quit();
+		return rsString;
+	}
+
+	private static PhantomJSDriver getDriver() {
+		File phantomjs = Phanbedder.unpack();
+		DesiredCapabilities dcaps = new DesiredCapabilities();
+		dcaps.setCapability(
+				PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
+				phantomjs.getAbsolutePath());
+		PhantomJSDriver driver = new PhantomJSDriver(dcaps);
+		return driver;
+	}
+
+	private static String getSiteName(String url) {
+		String site = "";
+		int idxSt = url.indexOf("//");
+		int idxEd = url.indexOf("/", idxSt + 2);
+
+		idxSt = idxSt == -1 ? 0 : idxSt + 2;
+		site = idxEd == -1 ? url.substring(idxSt) : url.substring(idxSt, idxEd);
+
+		return site;
+
 	}
 }
